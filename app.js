ec2228e1c8e23e890b4b66dfedc4cd65f44f30a3
levintoo <tooklevin@gmail.com>
2021-07-29 16:07:02 +0300

    js classes

--- app.js
+++ app.js
@@ -9,5 +9,24 @@ const cartItems = document.querySelector('.cart-items');
 const cartTotal = document.querySelector('.cart-total');
 const cartContent = document.querySelector('.cart-content');
 const productsDOM = document.querySelector('.products-center');
+// cart
+let cart = [];
 
-let cart = []
+// getting the products
+class Products{
+
+}
+// display products
+class UI{
+
+}
+
+// local storage
+class Storage{
+
+}
+
+document.addEventListener('DOMContentLoaded', () =>{
+    const ui = new UI();
+    const products = new products();
+})
